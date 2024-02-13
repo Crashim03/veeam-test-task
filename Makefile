0512362ac2212ca64ed1550a55fdac80741e3186
Crashim03 <alexteloduarte@gmail.com>
2024-02-13 19:39:24 +0000

    save and load working

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
 run: 
 	python3 src/sync.py
 
-test:
-	python3 tests/sync_test.py
+test: clean
+	cp -r tests/assets tests/source_folder
+	python3 src/sync.py < tests/test1.txt
 
 clean:
 	rm -f src/data.json
--- src/sync.py
+++ src/sync.py
@@ -1,26 +1,60 @@
-from os import path
+from os import path, walk
 import json
+import threading
+import time
 
 SAVED_DATA_FOLDER = "data.json"
+INTERVAL = 1
 
+ACTIVE = True
 SOURCE_FOLDER = ''
 REPLICA_FOLDER = ''
+FILES_INFO = {}
 
 
-def set_origin_folder(source: str):
+def set_source_folder(source: str):
+    global SOURCE_FOLDER
     SOURCE_FOLDER = source
     # TODO More commands
 
 
 def set_replica_folder(replica: str):
+    global REPLICA_FOLDER
     REPLICA_FOLDER = replica
     # TODO More commands
+
+
+def set_files_info(files: dict):
+    global FILES_INFO
+    FILES_INFO = files
+    # TODO more commands
+
+
+def get_folder_info():
+    files_info = {}
+    for (root, dirs, files) in walk(SOURCE_FOLDER, topdown=True):
+        file_list = []
+        dir_list = []
+
+        for file in files:
+            file_list.append((file, path.getctime(path.join(root, file))))
+
+        for directory in dirs:
+            dir_list.append(
+                (directory, path.getctime(path.join(root, directory))))
+
+        files_info[root] = {
+            'directories': tuple(dir_list),
+            'files': tuple(file_list)
+        }
+    set_files_info(files_info)
 
 
 def save_data():
     data = {
         "source_folder": SOURCE_FOLDER,
-        "replica_folder": REPLICA_FOLDER
+        "replica_folder": REPLICA_FOLDER,
+        "files_info": FILES_INFO
     }
 
     with open(path.join(path.dirname(__file__), SAVED_DATA_FOLDER), "w") as outfile:
@@ -31,8 +65,9 @@ def load_data():
     with open(path.join(path.dirname(__file__), SAVED_DATA_FOLDER), "r") as openfile:
         data = json.load(openfile)
 
-    set_origin_folder(data["source_folder"])
+    set_source_folder(data["source_folder"])
     set_replica_folder(data["replica_folder"])
+    set_files_info(data["files_info"])
 
 
 def reset_data():
@@ -41,9 +76,34 @@ def reset_data():
 
 
 def sync():
+    while (ACTIVE):
+        get_folder_info()
+        save_data()
+        time.sleep(INTERVAL)
     # TODO
-    pass
 
 
 if (path.exists(path.join(path.dirname(__file__), SAVED_DATA_FOLDER))):
     load_data()
+
+if SOURCE_FOLDER == '':
+    set_source_folder(input("Type the folder you want to synchronize: "))
+    print()
+
+if REPLICA_FOLDER == '':
+    set_files_info(
+        input("Type the folder where you want to store the files: "))
+    print()
+
+print(SOURCE_FOLDER)
+
+thread = threading.Thread(target=sync)
+thread.start()
+
+while (ACTIVE):
+    command = input("Enter\n")
+
+    if command.lower() == "exit":
+        ACTIVE = False
+        thread.join()
+        break
